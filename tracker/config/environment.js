a6ef8be777f49041873e502c5eeecae0e8b1285a
SYJ <maplesyj@gmail.com>
2017-05-07 12:53:51 -0700

    drawing part. problem: socketio emit

--- tracker/config/environment.js
+++ tracker/config/environment.js
@@ -2,10 +2,13 @@
 
 module.exports = function(environment) {
   var ENV = {
-    modulePrefix: 'tracker',
+    modulePrefix: 'draw',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
+    'ember-websockets': {
+        socketIO: true
+    },
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
